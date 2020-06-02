1f2a83fca248c748bc731fb5752123129c1e0f52
Simon <simon.roland@actual-experience.com>
2020-06-02 16:21:26 +0100

    Completed Module

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,7 @@
-var index = require("./index.js");
-var LineEndings = index.LineEndings;
-var transformLineEnding = index.transformLineEnding;
 
-var helpText = require("./info.js");
-var help = helpText.help;
-var usage = helpText.usage;
+const {LineEndings, transformLineEnding} = require("./index.js");
+const {help, usage} = require("./info.js");
+
 
 // Display help text and exit if when someone passes `-h` or `--help`.
 if (process.argv.includes("-h") || process.argv.includes("--help")) {
@@ -13,7 +10,7 @@ if (process.argv.includes("-h") || process.argv.includes("--help")) {
 }
 
 // Gather input from stdin.
-const buffer = "";
+let buffer = "";
 
 process.stdin.on("data", data => {
   buffer += data;
@@ -21,7 +18,7 @@ process.stdin.on("data", data => {
 
 // When stdin closes, operate on the gathered input.
 process.stdin.on("end", () => {
-  const results;
+  let results;
 
   switch (process.argv.slice(-1)[0]) {
     case "CR":
